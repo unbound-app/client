1866be5290162abde784bbc33b460bce72fe5917
Mario P. <me@marioparaschiv.com>
2025-03-26 08:48:47 +0000

    fix(react crashes): polyfill intl format

--- src/api/i18n.ts
+++ src/api/i18n.ts
@@ -1,6 +1,4 @@
-import type { LocaleStrings } from '@typings/api/i18n';
 import { createLogger } from '@structures/logger';
-import { i18n } from '@api/metro/common';
 import CoreStrings from '@i18n';
 
 
@@ -13,62 +11,14 @@ export const state = {
 	messages: {},
 };
 
+// @TODO: Implement
 function initialize() {
-	state.locale = i18n.getLocale() ?? 'en-US';
-	i18n.on('locale', onChange);
+	state.locale = 'en-US';
 
-	// Add core strings
-	add(CoreStrings);
-}
-
-function addStrings(locale: string, strings: LocaleStrings) {
-	if (!state.locale) return;
-
-	state.messages[locale] ??= {};
-	Object.assign(state.messages[locale], strings);
-
-	i18n.loadPromise.then(inject);
-}
-
-function inject() {
-	if (!state.locale || !i18n?.getLocale) return;
-
-	const context = i18n._provider._context;
-
-	Object.assign(context.messages, state.messages[state.locale] ?? {});
-	Object.assign(context.defaultMessages, state.messages['en-US'] ?? {});
-}
-
-async function onChange(locale) {
-	state.locale = locale;
-
-	await i18n.loadPromise;
-	inject();
-}
-
-export function add(strings: LocaleStrings) {
-	if (typeof strings !== 'object' || Array.isArray(strings)) {
-		throw new Error(
-			'Locale strings must be an object with languages and strings.',
-		);
-	}
-
-	for (const locale in strings) {
-		addStrings(locale, strings[locale]);
-	}
-
-	return {
-		remove: () => {
-			const context = i18n._provider._context;
-
-			for (const locale in strings) {
-				for (const message of Object.keys(strings[locale])) {
-					delete context.defaultMessages[message];
-					delete context.messages[message];
-					delete i18n.Messages[message];
-				}
-			}
-		},
+	// Backwards compat, pre-i18n switch
+	// @ts-expect-error - @TODO Implement formatting when implementing new i18n
+	String.prototype.format = function () {
+		return this.toString();
 	};
 }
 
@@ -78,6 +28,8 @@ try {
 	Logger.error('Failed to initialize i18n:', e.message);
 }
 
-export const Strings = CoreStrings['en-US'];
-
-// i18n?.Messages ?? {};
+export const Strings = new Proxy({}, {
+	get(_, prop) {
+		return CoreStrings[state.locale]?.[prop] || 'MISSING_STRING';
+	}
+});
